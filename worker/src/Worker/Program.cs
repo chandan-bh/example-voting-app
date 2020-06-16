286828a270de969139ccfc48005f71a13fcc19ca
chandan-bh <66864880+chandan-bh@users.noreply.github.com>
2020-06-16 18:21:59 +0100

    Updated Program.cs
    Postgress parameter

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
+                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;"); //changed here
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
